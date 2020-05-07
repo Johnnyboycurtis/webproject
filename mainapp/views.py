3bd34f8904eae32ac3c97d381525f1edaf8744f4
jonathan <johnnyboycurtis@gmail.com>
2020-05-07 10:25:34 -0700

    openpyxl was removed and using xlwings to export

--- mainapp/views.py
+++ mainapp/views.py
@@ -14,45 +14,44 @@ def index(request):
     return render(request, 'mainapp/index.html', context=context)
 
 
-from openpyxl import Workbook
+import xlwings as xl
 from django.http import HttpResponse
-from openpyxl.styles import Font
+from django.http import FileResponse
+import io
 
 def export_data(request):
-    response = HttpResponse(content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = 'attachment; filename="Data.xlsx"'
+    #response = HttpResponse(content_type='application/vnd.ms-excel')
+    #response['Content-Disposition'] = 'attachment; filename="Data.xlsx"'
 
     # create workbook
-    wb = Workbook()
-    sheet = wb.active
+    wb = xl.Book()
+    sheet = wb.sheets[0]
 
     # stylize header row
     # 'id','title', 'quantity','pub_date'
       
-    c1 = sheet.cell(row = 1, column = 1) 
-    c1.value = "id"
-    c1.font = Font(bold=True)
+    sheet.range('A1').value = 'id'
+    sheet.range('A1').api.Font.Bold = True
     
-    c2 = sheet.cell(row= 1 , column = 2) 
-    c2.value = "title"
-    c2.font = Font(bold=True)
+    sheet.range('B1').value = 'title'
+    sheet.range('B1').api.Font.Bold = True
 
-    c3 = sheet.cell(row= 1 , column = 3) 
-    c3.value = "quantity"
-    c3.font = Font(bold=True)
+    sheet.range('C1').value = 'quantity'
+    sheet.range('C1').api.Font.Bold = True
 
-    c4 = sheet.cell(row= 1 , column = 4) 
-    c4.value = "pub_date"
-    c4.font = Font(bold=True)
+    sheet.range('D1').value = 'pub_date'
+    sheet.range('D1').api.Font.Bold = True
     
     # export data to Excel
     rows = models.Data.objects.all().values_list('id','category', 'quantity','pub_date',)
-    for row_num, row in enumerate(rows, 1):
+    for row_num, row in enumerate(rows, 2):
         # row is just a tuple
-        for col_num, value in enumerate(row):
-            c5 = sheet.cell(row=row_num+1, column=col_num+1) 
-            c5.value = value
+        x = ('A', 'B', 'C', 'D')
+        for loc, value in zip(x, row):
+            loc = loc + str(row_num)
+            sheet.range(loc).value = value
 
-    wb.save(response)
-
-    return response
+    wb.save("tmp/Data.xlsx")
+    wb.close()
+    buffer = open("tmp/Data.xlsx", 'rb')
+    return FileResponse(buffer, as_attachment=True, filename='Data.xlsx')
