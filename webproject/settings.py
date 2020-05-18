a913e5abc5d1485d3b46187bcac8384306679256
Jonathan <johnnyboycurtis@gmail.com>
2020-05-18 12:39:06 -0700

    commenting out STATIC_ROOT

--- webproject/settings.py
+++ webproject/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
