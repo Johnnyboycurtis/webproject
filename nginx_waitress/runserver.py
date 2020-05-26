c5f9354aa0e0cf8ade0a81f34ce640f999f4dc88
Jonathan <johnnyboycurtis@gmail.com>
2020-05-26 12:57:14 -0700

    minor edits for tutorial

--- nginx_waitress/runserver.py
+++ nginx_waitress/runserver.py
@@ -4,4 +4,4 @@ from webproject.wsgi import application
 # documentation: https://docs.pylonsproject.org/projects/waitress/en/stable/api.html
 
 if __name__ == '__main__':
-    serve(application, host = '0.0.0.0', port='8080')
+    serve(application, host = 'localhost', port='8080')
